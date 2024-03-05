7729532ec66793ae2d1d46e50b1ce8a7d259b4b9
Hamzaali146 <aaly.galy@gmail.com>
2024-03-05 10:10:53 -0800

    updated code

--- qr.py
+++ qr.py
@@ -7,13 +7,14 @@ while True:
     name=input("Enter Student name: ")
     roll = int(input("Write student rollno: "))
     section = input("Write student section: ")
+    msg = input("any message you want to type: ")
     qr = qrcode.QRCode(
     version=1,
     error_correction=qrcode.constants.ERROR_CORRECT_L,
     box_size=10,
     border=4,
     )
-    data =f"{name} \n{roll} \n{section}"
+    data =f"{name} \n{roll} \n{section}\n{msg}"
     qr.add_data(data)
     qr.make(fit=True)
     img = qr.make_image(fill_color="black", back_color="white")
