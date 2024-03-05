6dcd8d740ff31a1f8975fe1c00edc9009f29af9e
Hamzaali146 <aaly.galy@gmail.com>
2024-03-05 09:43:06 -0800

    qrcode_updated

---
+++ qr.py
@@ -0,0 +1,20 @@
+import qrcode
+count = 0
+while True:
+    ongo=input("want to add student:").upper()
+    if ongo =="N":
+        break
+    name=input("Enter Student name: ")
+    roll = int(input("Write student rollno: "))
+    section = input("Write student section: ")
+    qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+    )
+    data =f"{name} \n{roll} \n{section}"
+    qr.add_data(data)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save(f"Qrcode/qr_Code_{name}.png")
